e6f1da103c3730cd771950a1fd2290c6475ba521
Miguel Angel Romero <miguelromero717@gmail.com>
2021-01-10 19:47:05 -0500

    Add flutter_barcode_scanner package - Config package for iOS - Add scan action to the scan button

--- lib/src/widgets/scan_button.dart
+++ lib/src/widgets/scan_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 class ScanButton extends StatelessWidget {
 
   @override
@@ -7,7 +9,10 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       elevation: 0,
       child: Icon(Icons.filter_center_focus),
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        print(barcodeScanRes);
+      },
     );
   }
 }
